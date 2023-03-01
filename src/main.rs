c82e20d1e6baafd6c38bf6b4ad299546a710b6cb
mennatabuelnaga <mennat.abuelnaga@gmail.com>
2023-03-01 16:40:43 +0200

    feat: init

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-
 use bip39::{Language, Mnemonic, Seed};
 use sha2::Sha256;
 use hkdf::Hkdf;
 use hex_literal::hex;
+use ed25519_dalek::{SECRET_KEY_LENGTH, SecretKey, PublicKey};
 fn main(){
     let mnemonic = Mnemonic::from_phrase("club fame frame deposit luxury derive rely upgrade abuse school town dog", Language::English).unwrap(); // 12 words = (128 bits + 4 checksum bits)/11 bits
 	println!("mnemonic {:?}", mnemonic.entropy()); // 128 bits = 16 bytes
@@ -12,12 +12,12 @@ fn main(){
     let info = hex!("f0f1f2f3f4f5f6f7f8f9");
     let hk = Hkdf::<Sha256>::from_prk(&seed.as_bytes()).expect("PRK should be large enough");
 
-	let mut okm = [0u8; 32];
+	let mut okm = [0u8; SECRET_KEY_LENGTH];
 	hk.expand(&info, &mut okm)
 		.expect("32 is a valid length for Sha256 to output");
     println!("okm {:?}", okm);
-    let sk = bn254::PrivateKey::try_from(okm.as_slice()).unwrap();
-    let pk = bn254::PublicKey::from_private_key(&sk);
+    let sk = SecretKey::from_bytes(okm.as_slice()).unwrap();
+    let pk: PublicKey = (&sk).into();
     println!("pk {:?}", pk);
 
 
